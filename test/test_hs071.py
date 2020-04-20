307f8c6932e527ae5e9cfdbd22771a5ecdcc2480
Rob Falck <rfalck@nasa.gov>
2020-04-20 08:26:06 -0400

    added tests for IPOPT solInform

--- test/test_hs071.py
+++ test/test_hs071.py
@@ -35,19 +35,20 @@ def sens(xdict, funcs):
 
 class TestHS71(unittest.TestCase):
 
-    def optimize(self, optName, optOptions={}, storeHistory=False, places=5):
+    def optimize(self, optName, optOptions={}, storeHistory=False, places=5, xScale=1.0,
+                 objScale=1.0, conScale=1.0, check_solution=True):
         # Optimization Object
         optProb = Optimization('HS071 Constraint Problem', objfunc)
 
         # Design Variables
         x0 = [1.0, 5.0, 5.0, 1.0]
-        optProb.addVarGroup('xvars', 4, lower=1, upper=5, value=x0)
+        optProb.addVarGroup('xvars', 4, lower=1, upper=5, value=x0, scale=xScale)
 
         # Constraints
-        optProb.addConGroup('con', 2, lower=[25, 40], upper=[None, 40])
+        optProb.addConGroup('con', 2, lower=[25, 40], upper=[None, 40], scale=conScale)
 
         # Objective
-        optProb.addObj('obj')
+        optProb.addObj('obj', scale=objScale)
 
         # Optimizer
         try:
@@ -58,19 +59,25 @@ class TestHS71(unittest.TestCase):
         sol = opt(optProb, sens=sens)
 
         # Check Solution
-        self.assertAlmostEqual(sol.objectives['obj'].value, 17.0140172, places=places)
+        if check_solution:
+            self.assertAlmostEqual(sol.objectives['obj'].value, 17.0140172, places=places)
 
-        self.assertAlmostEqual(sol.variables['xvars'][0].value, 1.0, places=places)
-        self.assertAlmostEqual(sol.variables['xvars'][1].value, 4.743, places=places)
-        self.assertAlmostEqual(sol.variables['xvars'][2].value, 3.82115, places=places)
-        self.assertAlmostEqual(sol.variables['xvars'][3].value, 1.37941, places=places)
+            self.assertAlmostEqual(sol.xStar['xvars'][0], 1.0, places=places)
+            self.assertAlmostEqual(sol.xStar['xvars'][1], 4.743, places=places)
+            self.assertAlmostEqual(sol.xStar['xvars'][2], 3.82115, places=places)
+            self.assertAlmostEqual(sol.xStar['xvars'][3], 1.37941, places=places)
 
-        if hasattr(sol, 'lambdaStar'):
-            self.assertAlmostEqual(sol.lambdaStar['con'][0], 0.55229366, places=places)
-            self.assertAlmostEqual(sol.lambdaStar['con'][1], -0.16146857, places=places)
+            if hasattr(sol, 'lambdaStar'):
+                self.assertAlmostEqual(sol.lambdaStar['con'][0], 0.55229366, places=places)
+                self.assertAlmostEqual(sol.lambdaStar['con'][1], -0.16146857, places=places)
+
+        return sol
 
     def test_snopt(self):
         self.optimize('snopt')
+
+    def test_snopt_scaling(self):
+        self.optimize('snopt', objScale=4.2, xScale=[2,3,4,5], conScale=[0.6, 1.7])
 
     def test_slsqp(self):
         self.optimize('slsqp')
@@ -79,7 +86,23 @@ class TestHS71(unittest.TestCase):
         self.optimize('nlpqlp')
 
     def test_ipopt(self):
-        self.optimize('ipopt')
+        opts = {}
+        opts['print_level'] = 5
+        opts['linear_solver'] = 'mumps'
+        sol = self.optimize('ipopt', optOptions=opts)
+        self.assertEqual(sol.optInform['value'], 0)
+        self.assertEqual(sol.optInform['text'], 'Solve Succeeded')
+        # Test that the inform is -1 when iterations are too limited.
+        opts['max_iter'] = 1
+        sol = self.optimize('ipopt', optOptions=opts, check_solution=False)
+        self.assertEqual(sol.optInform['value'], -1)
+        self.assertEqual(sol.optInform['text'], 'Maximum Iterations Exceeded')
+        # Test that the inform is -4 when max_cpu_time are too limited.
+        opts['max_iter'] = 100
+        opts['max_cpu_time'] = 0.001
+        sol = self.optimize('ipopt', optOptions=opts, check_solution=False)
+        self.assertEqual(sol.optInform['value'], -4)
+        self.assertEqual(sol.optInform['text'], 'Maximum CpuTime Exceeded')
 
     def test_conmin(self):
         opts = {'DELFUN' : 1e-9,
